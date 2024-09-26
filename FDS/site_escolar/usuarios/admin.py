c421fe1efea69277c702cd9ee9178bf4474527b1
Pedro Gusmao <padg@cesar.school>
2024-09-25 21:31:56 -0300

    solicitações no admin django

--- FDS/site_escolar/usuarios/admin.py
+++ FDS/site_escolar/usuarios/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Usuario
 from .models import Documento
+from .models import Solicitacao
 
 admin.site.register(Usuario)
 admin.site.register(Documento)
+admin.site.register(Solicitacao)
